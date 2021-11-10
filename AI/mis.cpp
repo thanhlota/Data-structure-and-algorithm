1d88a7df3f18590864ec398801509ff533760e75
Thanh Ta <borntobeagymer7@gmail.com>
2021-11-10 20:03:08 +0700

    change mis.cpp

--- AI/mis.cpp
+++ AI/mis.cpp
@@ -1,13 +1,18 @@
-
 #include <iostream>
-#include <string.h>
 using namespace std;
-int tss(int a=100);
-int main() {
-   int a=10;
-   cout<<tss();
-   system("pause");
-}
-int tss(int a){
-   return a;
+int main()
+{
+   int x, y, z;
+   cout << "Enter three integers:" << endl;
+   cin >> x;
+   cin >> y;
+   cin >> z;
+   int *ptr1, *ptr2, *ptr3;
+   ptr1 = &x;
+   ptr2 = &y;
+   ptr3 = &z;
+   cout << "The three integers are:" << endl;
+   cout << "x = " << *ptr1 << endl;
+   cout << "y = " << *ptr2 << endl;
+   cout << "z = " << *ptr3 << endl;
 }
